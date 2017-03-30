4c367ac5d7192cfdab7f7da6eb866bebffe7a74e
Bruno Donath <cin_bdonath@uolinc.com>
2017-03-30 17:58:53 -0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # reviewdepratos
+
+disponível pra download em: https://play.google.com/store/apps/details?id=com.ionicframework.dishing
--- www/js/settings.js
+++ www/js/settings.js
@@ -2,9 +2,9 @@
 //don't touch, automatically created by gulp
 angular.module("starter.settings", [])
   .constant('$constants', {
-    appUrl: 'https//dishing-dev.firebaseio.com',
-    awsUrl: 'https://dishing-test.s3-us-west-2.amazonaws.com/',
-    awsBucket: 'dishing-test',
-    awsAccess_key: 'AKIAJAU3SI6NCOUTBUWQ',
-    awsSecret_key: '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'
+    appUrl: '<inserir>',
+    awsUrl: '<inserir>',
+    awsBucket: '<inserir>',
+    awsAccess_key: <inserir>
+    awsSecret_key: '<inserir>'
   });
